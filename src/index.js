48a189f5fe8602861c6dbf2ed0eed01c4c406cab
Andrew <ankara7979@gmail,com>
2021-03-15 15:02:58 +0200

    feat: add solution function min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(!array || array.length === 0) {
+    return 0;
+  }
+  array.sort((a,b) => {
+    return a - b
+  })
+  return array[0]
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!array || array.length === 0) {
+    return 0;
+  }
+  array.sort((a,b) => {
+    return b - a
+  })
+  return array[0]       
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!array || array.length === 0) {
+    return 0;
+  }
+  return array.reduce((acc, rec) => acc + rec) / array.length
 }
